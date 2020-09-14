836db1cabd4485aa4e735fa13ed49005e5f3b784
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-09-14 12:00:38 +0200

    update background with input value

--- backgroundGenerator/script.js
+++ backgroundGenerator/script.js
@@ -1,16 +1,35 @@
 var color1 = document.getElementById('color1');
 var color2 = document.getElementById('color2');
 var displayCssCode = document.querySelector('h1');
-
-document.body.style.background = 'linear-gradient( to right,'+color1.value,+',' + color2.value +');';
-
-console.log("script is linked");
+var body = document.querySelector('body');
 
 color1.addEventListener("input", function() {
-  displayCssCode.innerHTML = "" + color1.value + "," + color2.value;
+  displayCssCode.innerHTML =
+  "Current CSS Background : linear-gradient(to right, "
+    +color1.value
+    +" , "
+    + color2.value
+    +")";
+  body.style.background =
+    "linear-gradient(to right, "
+    +color1.value
+    +","
+    + color2.value
+    +")";
 });
 
 color2.addEventListener("input", function() {
-  displayCssCode.innerHTML = "" + color1.value + "," + color2.value;
+  displayCssCode.innerHTML =
+  "Current CSS Background : linear-gradient(to right, "
+    +color1.value
+    +" , "
+    + color2.value
+    +")";
+  body.style.background =
+    "linear-gradient(to right, "
+    +color1.value
+    +","
+    + color2.value
+    +")";
 });
 
