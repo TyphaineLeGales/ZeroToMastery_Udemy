7115b58247bde46dcdf25b0e00a56696a11f570c
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-09-14 10:25:21 +0200

    click + keycode enter event

--- shoppingList/script.js
+++ shoppingList/script.js
@@ -4,9 +4,20 @@ var input = document.getElementById('userInput');
 
 btn.addEventListener('click', function () {
   if(input.value.length > 0) {
-    var li = document.createElement('li');
+     addListItem(input);
+  }
+});
+
+input.addEventListener('keypress', function (e) {
+  if(input.value.length > 0 && e.keyCode === 13) {
+    addListItem(input);
+  }
+
+});
+
+function addListItem(input) {
+  var li = document.createElement('li');
     li.appendChild(document.createTextNode(input.value));
     list.appendChild(li);
     input.value = "";
-  }
-});
+}
