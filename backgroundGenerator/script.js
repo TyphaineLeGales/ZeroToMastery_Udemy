1feabb0e3de8428dd87cd0c502bd46097bb78fdf
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-09-14 11:41:43 +0200

    input evnt

--- backgroundGenerator/script.js
+++ backgroundGenerator/script.js
@@ -3,8 +3,14 @@ var color2 = document.getElementById('color2');
 var displayCssCode = document.querySelector('h1');
 
 document.body.style.background = 'linear-gradient( to right,'+color1.value,+',' + color2.value +');';
-displayCssCode.innerHTML = "" + color1.value + "," + color2.value;
+
 console.log("script is linked");
 
+color1.addEventListener("input", function() {
+  displayCssCode.innerHTML = "" + color1.value + "," + color2.value;
+});
 
+color2.addEventListener("input", function() {
+  displayCssCode.innerHTML = "" + color1.value + "," + color2.value;
+});
 
