04a101d213be474d257a2523d56fa58f636cb41a
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-09-14 10:41:58 +0200

    toggle done onclick

--- shoppingList/script.js
+++ shoppingList/script.js
@@ -1,6 +1,7 @@
 var btn = document.getElementById('enter');
 var list = document.querySelector('ul');
 var input = document.getElementById('userInput');
+var listItems = document.querySelectorAll('li');
 
 btn.addEventListener('click', function () {
   if(input.value.length > 0) {
@@ -15,9 +16,20 @@ input.addEventListener('keypress', function (e) {
 
 });
 
+listItems.forEach(function(listItem, index) {
+  listItem.addEventListener("click", function () {
+    markAsDone(listItems[index]);
+  });
+})
+
+
 function addListItem(input) {
   var li = document.createElement('li');
     li.appendChild(document.createTextNode(input.value));
     list.appendChild(li);
     input.value = "";
+}
+
+function markAsDone (el) {
+  el.classList.toggle('done');
 }
