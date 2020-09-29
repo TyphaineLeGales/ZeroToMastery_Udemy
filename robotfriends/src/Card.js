f320fd4b57af955cb8da44eb7a4d7537568a0d88
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-09-29 10:21:11 +0200

    destructuring objects + API template string with prop.name

--- robotfriends/src/Card.js
+++ robotfriends/src/Card.js
@@ -3,14 +3,15 @@ import './Card.css';
 import 'tachyons';
 
 const Card = (props) => {
-		return (
-			<div className="bg-washed-yellow br2 card grow ma2 shadow-5">
-				<img src="https://robohash.org/{props.name}?size=200x200" alt="robot illustration"/>
-				<div className = "center">
-			   	<h1 className="name">{props.name}</h1>
-			    <p className="mail">{props.email}</p>
-				</div>
+	const {email, name} = props;
+	return (
+		<div className="bg-washed-yellow br2 card grow ma2 shadow-5 tc">
+			<img src={`https://robohash.org/${name}?size=200x200`} alt='robot ilustration'/>
+			<div className = "center">
+			  <h1 className="name">{name}</h1>
+			  <p className="mail">{email}</p>
 			</div>
-		)
+		</div>
+	)
 }
 export default Card
