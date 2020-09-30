4f6328f1cdde00410af6083004e750fff383a98b
Typhaine Le Gales <tiphaine.legales@neuf.fr>
2020-09-30 15:02:08 +0200

    fixed implicit return one line arrow function

--- robotfriends/src/App.js
+++ robotfriends/src/App.js
@@ -18,9 +18,9 @@ class App extends Component {
 	componentDidMount() { //lifecycle method that comes with any react component 
 		//fetch data
 		fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response => (return response.json())
+		.then(response => response.json())
 		.then(users => this.setState({robots:users}));
-		
+
 		console.log("didMount");
 	}
 
